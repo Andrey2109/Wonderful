ebfa60753c06998e4cb307dcfb189acc25ca2d50
Andrey2109 <python2109@gmail.com>
2025-09-12 11:28:43 +0300

    feat: Craeete .env file for security of keys -add .gitignore -print for checking everything works

---
+++ utils.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	APIKey string
+}
+
+func loadEnvVariables() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := Config{
+		APIKey: os.Getenv("API_KEY"),
+	}
+
+	if config.APIKey == "" {
+		log.Fatal("API_KEY is required")
+	}
+	fmt.Println(config)
+	return config
+}
